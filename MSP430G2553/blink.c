0f0b9425fe8c67e99a58665b6ec4eb2f0ea44a39
petr-shumilov <petr1shum@gmail.com>
2016-10-03 03:21:33 +0700

    Final version with timer and button interrupts

--- MSP430G2553/blink.c
+++ MSP430G2553/blink.c
@@ -3,45 +3,64 @@
 #define LED1 BIT0
 #define BUTTON1 BIT3
 
+#define BLINK_DELAY 12500
 
-static void sleep(volatile unsigned int t); // for delay
+// LED1 status
+int enabled = 1;
 
 void main()
 {
-	WDTCTL = WDTPW | WDTHOLD;		// Stop watchdog
-	P1DIR |= LED1; 					// Set P1.0 to output
-	P1REN |= BUTTON1; 				// Set P1.3 to input
+	WDTCTL = WDTPW | WDTHOLD; // Stop watchdog
 
+	// Cleaning everything
+	P1OUT &= 0x0;
+	P1DIR &= 0x0;
 
-	int enabled = 1; // LED1 status
-	for(;;)
-	{
+	// Enable LED1
+	P1DIR |= LED1; 	// set P1.0 to output
+	P1OUT |= BIT0;
 
-		// if BUTTON1 pressed
-		while ((P1IN & BUTTON1) == 0)
-		{
-			if (enabled == 1)
-			{
-				P1OUT &= ~LED1; // toggle LED1
-				enabled = 0; // change LED1 status
-				sleep(40000); // waiting anti-bounce
-				break;
-			}
-			else
-			{
-				enabled = 1; // change LED1 status to enable
-				sleep(40000);
-				break;
-			}
-		}
-		if (enabled)
-			P1OUT ^= BIT0; // Toggle P1.0 using exclusive-OR
-		sleep(10000);
-	}
+	// Configure BUTTON1
+	P1REN |= BUTTON1;
+	P1OUT |= BUTTON1;
+	P1IE |= BUTTON1; // BUTTON1 interrupt enable
+	P1IES |= BUTTON1;
+	P1IFG &= ~BUTTON1;
+
+	// Configure timer
+	TACCTL0 |= CCIE;		// enable interrupts on timer
+	TACCR0 = BLINK_DELAY;	// number of delay cycles
+	TACTL |= TASSEL_1;
+	TACTL |= MC_1;
+
+	// shut down CPU while interrupts
+	__bis_SR_register(CPUOFF | GIE);
 }
 
-void sleep(volatile unsigned int t)
+
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer_A0(void)
 {
-	do t--;
-	while(t != 0);
+	TACCTL1 &= ~CCIFG;
+	if (enabled)
+		P1OUT ^= LED1;	// blinking the LED
+}
+
+#pragma vector=PORT1_VECTOR
+__interrupt void Port_1(void)
+{
+	// changing LED1 status
+	if (enabled == 1)
+	{
+		enabled = 0;
+		P1OUT &= ~LED1; // toggle LED1
+	}
+	else if (enabled == 0)
+	{
+		enabled = 1;
+		P1OUT &= ~LED1; //  LED1
+	}
+	// delay while
+	__delay_cycles(1000);
+	P1IFG = 0x0;
 }
